7fb641adfc6112e08bfbc37d90a0744b900db80f
Alejandro3469 <alejandro.perez3469@gmail.com>
2021-02-07 19:25:56 -0600

    Update navigationDrawer.js

--- scripts/navigationDrawer.js
+++ scripts/navigationDrawer.js
@@ -1,8 +1,8 @@
 function show() {
-    var element = document.getElementById("drawer");
-    var denier = document.getElementById("denier");
-    element.style.left= "0";
-    denier.style.display= "block";
+  var element = document.getElementById("drawer");
+  var denier = document.getElementById("denier");
+  element.style.left= "0";
+  denier.style.display= "block";
 }
 function hide() {
     var element = document.getElementById("drawer");
